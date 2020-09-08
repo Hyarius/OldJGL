fbdde7d6ed8d1c8314b5e73776e1bfc943600219
Hyarius <Boissy.jeremy@yahoo.fr>
2020-09-08 22:50:09 +0200

    Secure push

--- srcs/structure/jgl_application.cpp
+++ srcs/structure/jgl_application.cpp
@@ -228,7 +228,7 @@ namespace jgl
 
 	void Application::clear()
 	{
-		glClearColor(0.8f, 0.1f, 0.1f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
