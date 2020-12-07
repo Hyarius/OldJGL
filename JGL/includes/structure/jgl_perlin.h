49d5e6822f8767b36820bfed045d4eae72bb0e0f
Hyarius <31560599+Hyarius@users.noreply.github.com>
2020-12-07 16:01:23 +0100

    Update jgl_perlin.h
    Editing perlin

--- JGL/includes/structure/jgl_perlin.h
+++ JGL/includes/structure/jgl_perlin.h
@@ -1,88 +1,121 @@
-#ifndef JGL_PERLIN_H
-#define JGL_PERLIN_H
+class Perlin {
+private:
+	std::uint8_t p[512];
+	
+	double fade(double t)
+	{
+		return t * t * t * (t * (t * 6 - 15) + 10);
+	}
 
-#include <vector>
-#include <random>
-#include <algorithm>
-#include <windows.h>
-#include <numeric>
+	double lerp(double t, double a, double b)
+	{
+		return a + t * (b - a);
+	}
 
-namespace jgl
-{
-	class Perlin {
-	private:
-		std::vector<int> p;
+	double grad(std::uint8_t hash, double x, double y, double z)
+	{
+		const std::uint8_t h = hash & 15;
+		const double u = h < 8 ? x : y;
+		const double v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+		return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+	}
 
-		double fade(double t)
+	double weight(std::int32_t octaves)
+	{
+		double amp = 1;
+		double value = 0;
+
+		for (std::int32_t i = 0; i < octaves; ++i)
 		{
-			return t * t * t * (t * (t * 6 - 15) + 10);
+			value += amp;
+			amp /= 2;
 		}
 
-		double lerp(double t, double a, double b)
+		return value;
+	}
+
+public:
+	Perlin(size_t seed = std::default_random_engine::default_seed)
+	{
+		reseed(seed);
+	}
+	
+	void reseed(std::uint32_t seed)
+	{
+		for (size_t i = 0; i < 256; ++i)
 		{
-			return a + t * (b - a);
+			p[i] = static_cast<std::uint8_t>(i);
 		}
 
-		double grad(int hash, double x, double y, double z)
+		std::shuffle(std::begin(p), std::begin(p) + 256, std::default_random_engine(seed));
+
+		for (size_t i = 0; i < 256; ++i)
 		{
-			int h = hash & 15;
-			double u = h < 8 ? x : y,
-				v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-			return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+			p[256 + i] = p[i];
+		}
+	}
+
+	//Noise [0, 1]
+	double noise(double x, double y = 0, double z = 0)
+	{
+		const std::int32_t X = static_cast<std::int32_t>(std::floor(x)) & 255;
+		const std::int32_t Y = static_cast<std::int32_t>(std::floor(y)) & 255;
+		const std::int32_t Z = static_cast<std::int32_t>(std::floor(z)) & 255;
+
+		x -= std::floor(x);
+		y -= std::floor(y);
+		z -= std::floor(z);
+
+		const double u = fade(x);
+		const double v = fade(y);
+		const double w = fade(z);
+
+		const std::int32_t A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z;
+		const std::int32_t B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;
+			
+		return (lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z),
+			grad(p[BA], x - 1, y, z)),
+			Lerp(u, grad(p[AB], x, y - 1, z),
+			grad(p[BB], x - 1, y - 1, z))),
+			lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1),
+			grad(p[BA + 1], x - 1, y, z - 1)),
+			lerp(u, grad(p[AB + 1], x, y - 1, z - 1),
+			grad(p[BB + 1], x - 1, y - 1, z - 1)))) + 1) / 2;
+	}
+	
+	double accumulated_noise(int32_t  octaves, double x, double y = 0, double z = 0)
+	{
+		double result = 0;
+		double amp = 1;
+
+		for (int32_t  i = 0; i < octaves; ++i)
+		{
+			result += noise(x, y, z) * amp;
+			x *= 2.0;
+			y *= 2.0;
+			z *= 2.0;
+			amp /= 2.0;
 		}
 
-	public:
-		Perlin()
+		return (result / weight(octaves));
+	}
+	
+	void serialize(std::array<std::uint8_t, 256>& s)
+	{
+		for (std::size_t i = 0; i < 256; ++i)
 		{
-			p = {
-				151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
-				8,99,37,240,21,10,23,190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,
-				35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,
-				134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,
-				55,46,245,40,244,102,143,54, 65,25,63,161,1,216,80,73,209,76,132,187,208, 89,
-				18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,
-				250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,
-				189,28,42,223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167,
-				43,172,9,129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,
-				97,228,251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,
-				107,49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-				138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 };
-			p.insert(p.end(), p.begin(), p.end());
+			s[i] = p[i];
 		}
-		Perlin(unsigned int seed)
+	}
+
+	void deserialize(const std::array<std::uint8_t, 256>& s)
+	{
+		for (std::size_t i = 0; i < 256; ++i)
 		{
-			p.resize(256);
-			std::iota(p.begin(), p.end(), 0);
-			std::default_random_engine engine(seed);
-			std::shuffle(p.begin(), p.end(), engine);
-			p.insert(p.end(), p.begin(), p.end());
+			p[256 + i] = p[i] = s[i];
 		}
-		double noise(double x, double y, double z)
-		{
-			int X = (int)floor(x) & 255;
-			int Y = (int)floor(y) & 255;
-			int Z = (int)floor(z) & 255;
-
-			x -= floor(x);
-			y -= floor(y);
-			z -= floor(z);
-
-			double u = fade(x);
-			double v = fade(y);
-			double w = fade(z);
-
-			int A = p[X] + Y;
-			int AA = p[A] + Z;
-			int AB = p[A + 1] + Z;
-			int B = p[X + 1] + Y;
-			int BA = p[B] + Z;
-			int BB = p[B + 1] + Z;
-
-			double res = lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), grad(p[BA], x - 1, y, z)), lerp(u, grad(p[AB], x, y - 1, z), grad(p[BB], x - 1, y - 1, z))), lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1), grad(p[BA + 1], x - 1, y, z - 1)), lerp(u, grad(p[AB + 1], x, y - 1, z - 1), grad(p[BB + 1], x - 1, y - 1, z - 1))));
-			return (res + 1.0) / 2.0;
-		}
-	};
-}
+	}
+};
 
 extern jgl::Perlin* g_perlin;
 
